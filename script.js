b6c766ece94ecb3b28fe955769ed6cedd916c742
Souvik Guha <svkguha123@gmail.com>
2023-02-27 14:27:24 +0530

    push

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function backspace(){
+    let myinput = document.getElementById("calc").value;
+    let ans = myinput.substring(0,myinput.length-1);
+    console.log(ans);
+    document.getElementById("calc").value = ans;
+}
